625a7f006066dda5fc899546410abc2e347ab7d2
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-09-05 04:45:08 -0500

    Implementando ejemplos de la seccion 1.c

--- part1/app_introduccion/src/App.jsx
+++ part1/app_introduccion/src/App.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+
+/* Primer ejercicio de introduccion modificado
 const Hello = (props) => {
   const {name, age} = props
   const bornYear = () => new Date().getFullYear() - age
@@ -19,6 +22,54 @@ const App = () => {
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
+    </div>
+  )
+}
+*/
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onSmash, text}) => <button onClick={onSmash}>{text}</button>
+
+const App = () => {
+  const[counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  /*setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+    
+  console.log('rendering...', counter)*/
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
+  return (
+    <div>
+      <Display counter={counter}/>
+      <Button
+        onSmash={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onSmash={setToZero}
+        text='zero'
+      />
+      <Button
+        onSmash={decreaseByOne}
+        text='minus'
+      />
     </div>
   )
 }
--- part1/app_introduccion/src/main.jsx
+++ part1/app_introduccion/src/main.jsx
@@ -3,3 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+/*
+let counter = 1
+
+const refresh = () => {
+    ReactDOM.createRoot(document.getElementById('root')).render(
+        <App counter={counter} />
+    )
+}
+
+setInterval(() =>{
+    refresh()
+    counter += 1
+}, 1000)
+*/

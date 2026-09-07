974751a22e1eac5eb0c56125c3efbef4971e4fec
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-09-07 00:01:34 -0500

    Optimizacion con hooks de ejemplo

--- part1/app_introduccion/src/App.jsx
+++ part1/app_introduccion/src/App.jsx
@@ -76,6 +76,27 @@ const App = () => {
 }
 */
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
@@ -99,18 +120,14 @@ const App = () => {
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-      <button onClick={handleRightClick}>
-        right
-      </button>
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
       {right}
-      <p>{allClicks.join(' ')}</p>
-      <p>total {total}</p>
+      <History allClicks={allClicks} />
     </div>
   )
 }
+
 
 /*
 const App = () => {
@@ -131,6 +148,6 @@ const App = () => {
     </div>
   )
 }
-  */
+*/
 
 export default App

2a69f1c96ccb221bcd114f89a1847766e9c32eed
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-09-08 02:49:36 -0500

    Implementacion de quinto ejercicion de la seccion 1.d

--- part1/app_introduccion/src/App.jsx
+++ part1/app_introduccion/src/App.jsx
@@ -168,6 +168,14 @@ const App = () => {
 }
 */
 
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+
 const App = () => {
   const [value, setValue] = useState(10)
 
@@ -178,16 +186,10 @@ const App = () => {
 
   return (
     <div>
-      {value}
-      <button onClick={() => setToValue(1000)}>
-        thousand
-      </button>
-      <button onClick={() => setToValue(0)}>
-        reset
-      </button>
-      <button onClick={() => setToValue(value + 1)}>
-        increment
-      </button>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }

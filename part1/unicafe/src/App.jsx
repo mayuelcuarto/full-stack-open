d7d0fe9e77f6092d651714e1b14f18bd81e0ae1a
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-09-08 04:25:32 -0500

    Solucion del ejercicio 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,16 +4,30 @@ const Display = ({text, counter, unit}) => <div>{text} {counter} {unit}</div>
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
+const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad
+  // En esta parte agregue una condicional para total y asi evitar el NaN al cargar la pagina
+  const average = total === 0 ? 0 : (props.good * 1 + props.bad * -1) / total
+  const positive = total === 0 ? 0 : (props.good * 100) / total
+
+  return(
+    <div>
+      <h1>statistics</h1>
+      <Display text="good" counter={props.good} />
+      <Display text="neutral" counter={props.neutral} />
+      <Display text="bad" counter={props.bad} />
+      <Display text="all" counter={total} />
+      <Display text="average" counter={average} />
+      <Display text="positive" counter={positive} unit="%" />
+    </div>
+  )
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const total = good + neutral + bad
-  // En esta parte agregue una condicional para total y asi evitar el NaN al cargar la pagina
-  const average = total === 0 ? 0 : (good * 1 + bad * -1) / total
-  const positive = total === 0 ? 0 : (good * 100) / total
   
   const increaseGoodByOne = () => {
     console.log('increasing, value before', good)
@@ -36,13 +50,7 @@ const App = () => {
       <Button onClick={increaseGoodByOne} text="good" />
       <Button onClick={increaseNeutralByOne} text="neutral" />
       <Button onClick={increaseBadByOne} text="bad" />
-      <h1>statistics</h1>
-      <Display text="good" counter={good} />
-      <Display text="neutral" counter={neutral} />
-      <Display text="bad" counter={bad} />
-      <Display text="all" counter={total} />
-      <Display text="average" counter={average} />
-      <Display text="positive" counter={positive} unit="%" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }

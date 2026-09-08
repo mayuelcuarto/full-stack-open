6179547be14c383959aec28f6c5a3c4cff5069e8
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-09-08 02:20:15 -0500

    Implementacion del quinto ejercicio de la seccion 1.d

--- part1/app_introduccion/src/App.jsx
+++ part1/app_introduccion/src/App.jsx
@@ -151,6 +151,7 @@ const App = () => {
 }
 */
 
+/* Cuarto ejercicio
 const App = () => {
   const [value, setValue] = useState(10)
 
@@ -162,6 +163,31 @@ const App = () => {
       <button onClick={hello('world')}>button</button>
       <button onClick={hello('react')}>button</button>
       <button onClick={hello('function')}>button</button>
+    </div>
+  )
+}
+*/
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+      {value}
+      <button onClick={() => setToValue(1000)}>
+        thousand
+      </button>
+      <button onClick={() => setToValue(0)}>
+        reset
+      </button>
+      <button onClick={() => setToValue(value + 1)}>
+        increment
+      </button>
     </div>
   )
 }

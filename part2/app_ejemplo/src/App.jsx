a0a006a8ea3f426b69cb21e8b81989190be6a993
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-09-26 05:00:22 -0500

    Actualizacion de ejemplo para seccion de Formularios

--- part2/app_ejemplo/src/App.jsx
+++ part2/app_ejemplo/src/App.jsx
@@ -1,13 +1,13 @@
-const App = (notes) => {
+import Note from './components/Note'
+
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
-          <li key={note.id}>
-            {note.content}
-          </li>
-        )}
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
       </ul>
     </div>
   )
